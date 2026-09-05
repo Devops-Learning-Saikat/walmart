3e7e0a1d08e032a69ab0b29561a1355732eccfeb
Saikat-1003 <saikat.g1996@gmail.com>
2026-09-05 10:55:15 +0000

    maven project

---
+++ oneapp/src/main/java/com/mycompany/App.java
@@ -0,0 +1,12 @@
+package com.mycompany;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("================================");
+        System.out.println("   Maven + Docker + Git Application");
+
+	}
+   }
+
